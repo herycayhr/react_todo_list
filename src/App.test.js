674423a4e12381e42581f1feebad21dcb9356a94
Heryca Hernandez <herycahernandez@Herycas-MacBook-Pro.local>
2020-02-08 11:43:33 -0600

    Fixing tests ;)

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('open app and renders todo input', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Todo Input/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('open app and renders todo list project', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Heryca's Todo List Project/i);
   expect(linkElement).toBeInTheDocument();
 });
